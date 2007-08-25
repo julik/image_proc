58657eb27a35e7733caf38254717c2a08c4bc0ad
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2007-08-25 00:17:54 +0000

    Recovery

--- image_proc.rb
+++ image_proc.rb
@@ -1,47 +1,188 @@
+# A simplistic interface to shell-based image processing. Pluggable, compact and WIN32-incompatible by
+# design. Bugger off.
+#
+#    width, height = ImageProc.get_bounds("image.png")
+#    thumb_filename = ImageProc.resize("image.png", "thumb.png", "50x50")
+require 'open3'
+
 class ImageProc
-  def self.process(from_path, to_path, geom_str)
-    @@processor ||= ImageProcConvert
-    @@processor.new.run(from_path, to_path, geom_str)
+  class Error < RuntimeError; end;
+  class FormatUnsupported < Error; end;
+
+  class << self
+
+    def engine; @@engine ||= detect_engine; end
+
+    def detect_engine
+      if RUBY_PLATFORM =~ /darwin/i
+        ImageProcSips
+      elsif (`which convert` =~ /^\// )
+        ImageProcConvert
+      else
+        raise "This system has no image processing facitilites that we can use"
+      end
+    end
+        
+    def get_bounds(of)
+      engine.new.get_bounds(File.expand_path(of))
+    end
+    
+    def method_missing(*args) #:nodoc:
+      engine.send(*args)
+    end
   end
   
-  def run(from_path, to_path, geom_str)
+  # Resize an image fitting the biggest side of it to the side of a square. A must for thumbs.
+  def resize_fit_square(from_path, to_path, square_side)
+    resize_fit_both(from_path, to_path, square_side, square_side)
+  end
+
+  # Resize an image fitting the boundary exactly. Will stretch and squash.
+  def resize_exact(from_path, to_path, geom_str)
+    validate_input(from_path, to_path, geom_str)
     @x, @y = geom_str.scan(/(\d+)/).flatten
     @y ||= @x
-    @source, @dest = [from_path, to_path].map{|p| File.expand_path(p) }
-    raise Errno::ENOENT, "No such file or directory #{@source}" unless File.exist?(@source)
-    raise Errno::ENOENT, "No such file or directory #{@dest}" unless File.exist?(File.dirname(@dest))
-    raise "This will overwrite #{@dest}" if File.exist?(@dest)
-    process
+
+    process_exact
   end
+
+  # Resize an image fitting it into a rect.
+  def resize_fit_both(from_path, to_path, w, h)
+    validate_input(from_path, to_path, w)
+    @x, @y = fit_sizes get_bounds(from_path), :width => width, :height => h
+    process_exact
+  end
+
+  # Resize an image fitting the biggest side of it to the side of a square. A must for thumbs.  
+  def resize_fit_width(from_path, to_path, width)
+    validate_input(from_path, to_path, width)
+    
+    @x, @y = fit_sizes get_bounds(from_path), :width => width
+    process_exact
+  end
+  
+  def resize_fit_height(from_path, to_path, height)
+    validate_input(from_path, to_path, height)
+    @x, @y = fit_sizes get_bounds(from_path), :height => height
+    process_exact
+  end
+  
+  alias_method :resize, :resize_exact
   
   private
-  
-  def raise_on_err(cmd)
-    stdin, stdout, stderr = Open3.popen3(cmd)
-    error = stderr.read.to_s.strip
-    raise "Could not convert the image using #{self.class}: #{error}" unless error.blank?
-    return stdout.read.strip
-  end
+    
+    def validate_input(from_path, to_path, arg)
+      @source, @dest = [from_path, to_path].map{|p| File.expand_path(p) }
+      @source_x, @source_y = get_bounds(from_path)
+
+      raise Errno::ENOENT, "No such file or directory #{@source}" unless File.exist?(@source)
+      raise Errno::ENOENT, "No such file or directory #{@dest}" unless File.exist?(File.dirname(@dest))
+      raise Error, "This will overwrite #{@dest}" if File.exist?(@dest)
+    end
+    
+    def integerize_values(h)
+      h.each_pair{|k,v| h[k] = v.to_i}
+    end
+    
+    def fit_sizes(bounds, opts)
+      integerize_values(opts)
+      
+      ratio = bounds[0].to_f / bounds[1].to_f
+      keys = opts.keys & [:width, :height]
+      floats = case keys
+        when [:width]
+          desired_w = opts[:width]
+          [desired_w, desired_w / ratio]
+        when [:height]
+          desired_h = opts[:height]
+          [desired_h * ratio, desired_h]
+        else # both, use reduction
+          smallest_side = [opts[:width], opts[:height]].sort.shift
+          if bounds[0] > bounds[1] # horizontal
+            fit_sizes bounds, :width => smallest_side
+          else
+            fit_sizes bounds, :height => smallest_side
+          end
+      end
+      
+      # Nudge output values to pixels
+      floats[0] = (floats[0].round > opts[:width] ? floats[0].ceil : floats[0].round) if opts[:width]
+      floats[1] = (floats[1].round > opts[:height] ? floats[1].ceil : floats[1].round) if opts[:height]
+      
+      floats.map{|v| v.round }
+    end
+    
+    def raise_on_err(cmd)
+      inp, outp, err = Open3.popen3(cmd)
+      error = err.read.to_s.strip
+
+      raise Error, "Problem with #{@source}: #{error}" unless error.nil? || error.empty?
+      result = outp.read.strip
+      [inp, outp, err].map{|socket| begin; socket.close; rescue IOError; end }
+      result
+    end
+    
 end
 
 class ImageProcConvert < ImageProc
-  def process
+  def process_exact
     raise_on_err("convert -scale #{@x}x#{@y} #{@source} #{@dest}")
   end
-end
-
-class ImageProcScience < ImageProc
-  def process
-    ImageScience.with_image(@source) do |img|
-      img.thumbnail(@x) {|t| thumb.save @dest }
-    end
+  
+  def process_width
+  end
+  
+  def process_height
+  end
+  
+  def process_fit
+  end
+  
+  def get_bounds(of)
+    raise_on_err("identify #{of}").scan(/(\d+)x(\d+)/)[0].map{|e| e.to_i }
   end
 end
 
 class ImageProcSips < ImageProc
-  def process
-    raise_on_err("sips -s format jpeg --resampleWidth #{@x} #{@source} --out #{@dest}")
+  # -Z pixelsWH --resampleHeightWidthMax pixelsWH
+  FORMAT_MAP = { ".tif" => "tiff", ".png" => "png", ".tif" => "tiff", ".gif" => "gif" }
+  
+  def process_exact
+    fmt = detect_source_format
+    raise_on_err("sips -s format #{fmt} --resampleHeightWidth #{@x} #{@y} #{@source} --out #{@dest}")
   end
+  
+  def process_fit_both(w, h)
+    bounds = get_bounds(@source)
+    
+  end
+  
+  def process_fit_width(width)
+  end
+  
+  def process_fit_height(height)
+  end
+  
+  def process_fit
+    fmt = detect_source_format
+    raise_on_err("sips -s format #{fmt} --resampleHeightWidthMax #{@x} #{@y} #{@source} --out #{@dest}")
+    return true
+  end
+  
+  def get_bounds(of)
+    raise_on_err("sips #{of} -g pixelWidth -g pixelHeight").scan(/(pixelWidth|pixelHeight): (\d+)/).to_a.map{|e| e[1].to_i}
+  end
+  
+  private
+    def detect_source_format
+      suspected = FORMAT_MAP[File.extname(@source)]
+      suspected =  (suspected.nil? || suspected.empty?) ? 'jpeg' : suspected
+      case suspected
+        when "png", "gif"
+          raise FormatUnsupported, "SIPS cannot resize indexed color GIF or PNG images, call Apple if you want to know why"
+      end
+      return suspected
+    end
 end
 
 require 'image_proc_test' if $0 == __FILE__
---
+++ image_proc_test.rb
@@ -0,0 +1,144 @@
+require 'test/unit'
+require 'fileutils'
+
+
+module ProcessorTest
+  INPUTS = File.expand_path(File.dirname(__FILE__) + '/input')
+  OUTPUTS = File.expand_path(File.dirname(__FILE__) + '/output')
+  
+  def setup
+    Dir.glob(File.dirname(__FILE__) + '/output/*.*').map{ |e| FileUtils.rm e }
+
+    @extensions = ["jpg", "png", "gif"]
+
+    @horizontals =  @extensions.map { | ext | "horizontal.#{ext}" }
+    @verticals =  @extensions.map { | ext | "vertical.#{ext}" }
+    @horizontal_bounds = 780, 520
+    @vertical_bounds = 466, 699
+  end
+  
+  def test_get_bounds_raise_when_file_passed_does_not_exist
+    assert_raise(ImageProc::Error) do
+      @processor.get_bounds("/tmp/__non_existent/#{Time.now.to_i}")
+    end
+  end
+  
+  def test_get_bounds_raise_when_file_passed_is_not_an_image
+    assert_raise(ImageProc::Error) do
+      @processor.get_bounds(INPUTS + '/not.an.image.tmp')
+    end
+  end
+  
+  def test_properly_detects_bounds
+    @horizontals.map do |file| 
+      assert_equal @horizontal_bounds, @processor.get_bounds(INPUTS + "/" + file)
+    end
+    
+    @verticals.map do |file| 
+      assert_equal @vertical_bounds, @processor.get_bounds(INPUTS + "/" + file)
+    end
+  end
+  
+  def test_resize_raises_when_trying_to_overwrite
+    assert_raise(ImageProc::Error) do
+      @processor.resize INPUTS + '/' + @horizontals[0], INPUTS + '/' + @horizontals[0], "100x100"
+    end
+  end
+  
+  def test_exact_resize
+    names = (@horizontals  + @verticals)
+    sources = names.map{|file| INPUTS + "/" + file }
+
+    sources.each_with_index do | source, index |
+      @processor.resize(source, OUTPUTS + '/' + names[index], "65x65")
+      result_p = OUTPUTS + '/' + File.basename(source)
+      assert File.exist?(result_p), "#{result_p} should have been created"
+      assert_equal [65, 65], get_bounds(result_p), "The image should have been resized exactly"
+    end
+  end
+  
+  # def test_exact_resize_raises_when_destination_directory_does_not_exist
+  # def test_resize_fit
+  # def test_resize_fit_width
+  # def test_resize_fit_height
+  
+  private
+    def get_bounds(of)
+      if RUBY_PLATFORM =~ /darwin/i
+        `sips #{of} -g pixelWidth -g pixelHeight`.scan(/(pixelWidth|pixelHeight): (\d+)/).to_a.map{|e| e[1]}
+      else
+        `identify #{of}`.scan(/(\d+)x(\d+)/)[0]
+      end.map{|e| e.to_i}
+    end
+end
+
+class TestGeometry < Test::Unit::TestCase
+  def setup
+    @x = ImageProc.new
+    class << @x
+      public :fit_sizes
+    end
+  end
+  
+  def test_fit_width
+    bounds = [1024, 500]
+    assert_equal [50, 24], @x.fit_sizes(bounds, :width => 50)
+
+    bounds = [500, 1024]
+    assert_equal [50, 102], @x.fit_sizes(bounds, :width => 50)
+  end
+
+  def test_fit_height
+    bounds = [1024, 500]
+    assert_equal [102, 50], @x.fit_sizes(bounds, :height => 50)
+
+    bounds = [500, 1024]
+    assert_equal [24, 50], @x.fit_sizes(bounds, :height => 50)
+  end
+  
+  def test_fit_both
+    bounds = [1024, 500]
+    assert_equal [70, 34], @x.fit_sizes(bounds, :height => 70, :width => 70)
+
+    bounds = [500, 1024]
+    assert_equal [34, 70], @x.fit_sizes(bounds, :height => 70, :width => 70)
+  end
+  
+  def test_we_grok_strings_too
+    bounds = [1024, 500]
+    assert_equal [70, 34], @x.fit_sizes(bounds, :height => "70", :width => "70")
+  end
+
+end
+
+class TestImageProcSips < Test::Unit::TestCase
+  def setup
+    super
+    @processor = ImageProcSips.new
+    @horizontals.reject!{|e| e =~ /\.(png|gif)/}
+    @verticals.reject!{|e| e =~ /\.(png|gif)/}
+  end
+  
+  def test_sips_does_not_grok_pngs
+    assert_raise(ImageProc::FormatUnsupported) do
+      @processor.resize(INPUTS + '/horizontal.gif', OUTPUTS + '/horizontal.gif', "100x100")
+    end
+  end
+  
+  def test_sips_does_not_grok_gifs
+    assert_raise(ImageProc::FormatUnsupported) do
+      @processor.resize(INPUTS + '/horizontal.png', OUTPUTS + '/horizontal.png', "100x100")
+    end
+  end
+  
+  include ProcessorTest
+end
+
+##class TestImageProcConvert < Test::Unit::TestCase
+##  def setup
+##    super
+##    @processor = ImageProcConvert.new
+##  end
+##  
+##  include ProcessorTest
+##end
